b21393e49ab69eec4527b4413bf87ac77a9f7e37
Nkatha004 <nkathajoy36@gmail.com>
2023-02-03 14:57:06 +0300

    Exam marks

---
+++ resources/views/exams/viewMarks.blade.php
@@ -0,0 +1,44 @@
+@include('dashboard.dashboardSideNav')
+<main>
+	<div class = "text-center table-schools">
+
+	<table class="table table-striped">
+			<thead>
+				<tr>
+					<th scope="col">Student Name</th>
+					<th scope="col">Class</th>
+                    <th scope="col">Year</th>
+                    <th scope="col">Term</th>
+                    <th scope="col">Subject</th>
+                    <th scope="col">Mark</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($marks as $mark)
+				<tr>
+			
+					<td>{{ App\Http\Controllers\StudentSubjectsController::getStudentName($mark->student_subject_id) }}</td>
+					<td>{{ App\Http\Controllers\StudentSubjectsController::getClass($mark->student_subject_id) }}</td>
+                    <td>{{ $mark->year }}</td>
+                    <td>{{ $mark->term }}</td>
+					<td>{{ App\Http\Controllers\StudentSubjectsController::getSubject($mark->student_subject_id) }}</td>
+                    <td>{{ $mark->mark }}</td>
+                    @if (Auth::user()->role_id == 2)
+					<td>
+						<a href = "{{ url('/editmark/'.$mark->id) }}" class = "btn btn-sm btn-warning">Update</a>
+						<a href = "{{ url('/deletemark/'.$mark->id) }}" class = "btn btn-sm btn-danger">Delete</a>
+					</td>
+                    @endif
+				</tr>
+				@endforeach
+                @if (Auth::user()->role_id != 1)
+                <tr>
+                    <td colspan = "6">
+                        <a href = "{{ url('/employeesubjects/'.Auth::user()->id) }}" class = "btn btn-sm btn-secondary">Back to Students</a>
+                    </td>
+                </tr>
+                @endif
+			</tbody>
+		</table>
+	</div>
+</main>
---
+++ resources/views/students/viewStudentstoAddMarks.blade.php
@@ -0,0 +1,82 @@
+@include('dashboard.dashboardSideNav')
+<main>
+	<div class = "text-center table-employees">
+	<table class="table table-striped">
+			<thead id = "viewstudents">
+				<tr>
+					<th scope="col">Admission Number</th>
+					<th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+
+					<!-- Display guardian phone number to all except admin  -->
+					@if (Auth::user()->role_id != 1)
+						<th scope="col">Phone Number</th>
+					@endif
+					
+					<th scope="col">Class</th>
+
+					<!-- Display school name to admin only -->
+					@if (Auth::user()->role_id == 1)
+					<th scope="col">School</th>
+					@endif
+				</tr>
+			</thead>
+			<tbody>
+				@if ($message ?? null)
+					<tr>
+						<td colspan = "5">{{$message}}</td>
+					</tr>
+					<tr>
+						<td colspan = "5">
+							@if ($message ?? null == 'No classes and students found yet!')
+							<div class = "text-center">
+								<a href="{{URL::to('/classes')}}"><button>Add New Class</button></a>
+							</div>
+							@else
+							<div class = "text-center">
+								<a href="{{URL::to('/students')}}"><button>Add New Student</button></a>
+							</div>
+							@endif
+						</td>
+					</tr>
+				@else
+					@foreach($students as $student)
+					<tr>
+						<td>{{ $student->admission_number }}</td>
+						<td>{{ $student->first_name }}</td>
+						<td>{{ $student->last_name }}</td>
+
+						<!-- Display phone number to all users except from admin -->
+						@if (Auth::user()->role_id != 1)
+						<td>{{ $student->guardian_phone_number }}</td>
+						@endif
+
+						<td>{{App\Http\Controllers\ClassesController::getClassName($student->class_id) }}</td>
+
+						<!-- Display school name only when admin is logged in -->
+						@if (Auth::user()->role_id == 1)
+							<td>{{App\Http\Controllers\SchoolsController::getSchoolNameByClassID($student->class_id) }}</td>
+						@endif
+						@if (Auth::user()->role_id != 3)
+						<td><a href = "{{ url('/studentsubjects/'.$student->id) }}" class = "btn btn-sm btn-info">Student Subjects</a></td>
+						@endif
+                        <td>
+							@if (Auth::user()->role_id != 3)
+							<a href = "{{ url('/editstudent/'.$student->id) }}" class = "btn btn-sm btn-warning">Update</a>
+								@if (Auth::user()->role_id != 4)
+								<a href = "{{ url('/deletestudent/'.$student->id) }}" class = "btn btn-sm btn-danger">Delete</a>
+								@endif
+                            @else
+                            <a href = "{{ url('/marks/'.$student->id.'/'.$subject) }}" class = "btn btn-sm btn-danger">Add Mark</a>
+							@endif
+							@if (Auth::user()->role_id == 4)
+								<a href = "{{ url('/marks/'.$student->id.'/'.$subject) }}" class = "btn btn-sm btn-danger">Add Mark</a>
+							@endif
+						</td>
+					</tr>
+					@endforeach
+				@endif
+			</tbody>
+		</table>
+	</div>
+</main>
