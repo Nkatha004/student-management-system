24c6e4e11093d78179a98fbf7763db06367fb5d4
Nkatha <69391540+Nkatha004@users.noreply.github.com>
2023-03-14 19:47:30 +0300

    Modified host

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'student-management-system-server'),
+            'host' => env('DB_HOST', 'student-management-system-server.mysql.database.azure.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'student-management-system-database'),
             'username' => env('DB_USERNAME', 'bnqjsqaqwz'),
